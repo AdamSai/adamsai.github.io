a423892ca9008edb5cff5336333838810f6ae8b0
Adam Saidane <adamsaidane252@gmail.com>
2021-10-28 16:37:26 +0200

    Add more projects to portfolio

--- src/data/GamesData.js
+++ src/data/GamesData.js
@@ -1,43 +1,51 @@
 const games = [
-	{
-		title: 'Happy Hour',
-		imgSrc: require('../img/portfolio/happy-hour.png'),
-		year: 2021,
-		description: `Happy Hour was made for Ludom Dare 49. The theme was 'unstable' and we decided
+  {
+    title: "Happy Hour",
+    imgSrc: require("../img/portfolio/happy-hour.png"),
+    year: 2021,
+    description: `Happy Hour was made for Ludom Dare 49. The theme was 'unstable' and we decided
     to make a beer serving game. You have to serve beers to customers before they get angry. The drunker
     they get, and the longer they have to wait, the likelier they are to start fighting. As the waiter
     you can also headbutt the patrons if you really feel like it. I worked on bug-fixing and refactoring 
     the AI after submission. I also tried out the new UI Toolkit for Unity.`,
-		sourceCode: 'https://github.com/MrAbnox/LD49',
-		projectLink: 'https://pedrorainha.itch.io/ld-49-happy-hour',
-	},
-	{
-		title: 'Crate Spider',
-		year: 2021,
-		imgSrc: require('../img/portfolio/crate-spiderpng.png'),
-		description: `Crate Spider is a challenging game inspired by SpeedRunners and Spider-Man. The
+    sourceCode: "https://github.com/MrAbnox/LD49",
+    projectLink: "https://pedrorainha.itch.io/ld-49-happy-hour",
+  },
+  {
+    title: "Crate Spider",
+    year: 2021,
+    imgSrc: require("../img/portfolio/crate-spiderpng.png"),
+    description: `Crate Spider is a challenging game inspired by SpeedRunners and Spider-Man. The
     goal of the game is to clear all levels as fast as possible. Once you get the hang of how to 
     keep your momentum the game is really fun. It was made as a prototype for the class Making Games
     at the IT University of Copenhagen. `,
-		projectLink: 'https://predmada.itch.io/crate-spider',
-	},
-	{
-		title: "Adam's Tower Defense",
-		year: 2019,
-		imgSrc: require('../img/portfolio/tower-defense.png'),
-		description: `My tower defense game was created as an assignment for the Games elective course
+    projectLink: "https://predmada.itch.io/crate-spider",
+  },
+  {
+    title: "Larry's Mod",
+    year: 2019,
+    imgSrc: require("../img/portfolio/larrys-rumble.png"),
+    description: `Larry's Mod was a multiplayer brawler game. You play as Larry the Lobster, and you have to
+		melee fight the other players. It was my first multiplayer game, and I used the mirror framework for networking.`,
+    projectLink: "https://www.youtube.com/watch?v=FL8F9671tTg",
+  },
+  {
+    title: "Adam's Tower Defense",
+    year: 2019,
+    imgSrc: require("../img/portfolio/tower-defense.png"),
+    description: `My tower defense game was created as an assignment for the Games elective course
     during my A.P. Degree. The task was to create a tower defense game, where if the path was blocked,
     the AI would destroy towers to make a new path.`,
-		projectLink: 'https://predmada.itch.io/adams-tower-defense',
-	},
-	{
-		title: 'Project Boost',
-		year: 2018,
-		imgSrc: require('../img/portfolio/boost.png'),
-		description: `Project Boost was my second ever Unity project. It was created as part of a 
+    projectLink: "https://predmada.itch.io/adams-tower-defense",
+  },
+  {
+    title: "Project Boost",
+    year: 2018,
+    imgSrc: require("../img/portfolio/boost.png"),
+    description: `Project Boost was my second ever Unity project. It was created as part of a 
     Udemy course to prepare me for my Games elective on the 4th Semester of my A.P. Degree.`,
-		projectLink: 'https://predmada.itch.io/project-boost',
-	},
+    projectLink: "https://predmada.itch.io/project-boost",
+  },
 ];
 
 export default games;
--- src/data/SoftwareData.js
+++ src/data/SoftwareData.js
@@ -1,84 +1,34 @@
 const softwareProjects = [
-	{
-		title: 'Quadtree',
-		year: 2020,
-		imgSrc: require('../img/portfolio/quadtree.png'),
-		description: `For personal research I decided to look into how the quadtree datastructure worked, 
+  {
+    title: "Quadtree",
+    year: 2020,
+    imgSrc: require("../img/portfolio/quadtree.png"),
+    description: `For personal research I decided to look into how the quadtree datastructure worked, 
     and I made a visual implementation in Unity.`,
-		projectLink: 'https://github.com/AdamSai/Quadtree',
-	},
-	{
-		title: 'Sample element',
-		imgSrc: require('../img/portfolio/programming.jpg'),
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-    unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-    the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-    with desktop publishing software like Aldus PageMaker including versions of Lorem
-    Ipsum.`,
-	},
-	{
-		title: 'Sample element',
-		imgSrc: require('../img/portfolio/programming.jpg'),
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-    unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-    the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-    with desktop publishing software like Aldus PageMaker including versions of Lorem
-    Ipsum.`,
-	},
-	{
-		title: 'Sample element',
-		imgSrc: require('../img/portfolio/programming.jpg'),
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-    unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-    the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-    with desktop publishing software like Aldus PageMaker including versions of Lorem
-    Ipsum.`,
-	},
-	{
-		title: 'Sample element',
-		imgSrc: require('../img/portfolio/programming.jpg'),
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-    unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-    the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-    with desktop publishing software like Aldus PageMaker including versions of Lorem
-    Ipsum.`,
-	},
-	{
-		title: 'Sample element',
-		imgSrc: require('../img/portfolio/programming.jpg'),
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-    unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-    the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-    with desktop publishing software like Aldus PageMaker including versions of Lorem
-    Ipsum.`,
-	},
-	{
-		title: 'Sample element',
-		imgSrc: require('../img/portfolio/programming.jpg'),
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-    Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-    unknown printer took a galley of type and scrambled it to make a type specimen book.
-    It has survived not only five centuries, but also the leap into electronic
-    typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-    the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-    with desktop publishing software like Aldus PageMaker including versions of Lorem
-    Ipsum.`,
-	},
+    sourceCode: "https://github.com/AdamSai/Quadtree",
+  },
+  {
+    title: "System Integration",
+    year: 2020,
+    imgSrc: require("../img/portfolio/si_exam.png"),
+    description: `For my System Integration exam at my bachelor's, we were tasked with creating
+    multiple microservices, which integrated with a legacy system. We also used RabbitMQ as a message broker
+    between some of the services and Kafka to aggregate data. My main responsibility was the monolithic legacy system
+    and setting up Camunda and BPMN. Linked is a repository explaining the project
+    and linking to the different services we developed.`,
+    sourceCode: "https://github.com/SOFTBoiS/SI_Exam_Repository",
+  },
+  {
+    title: "Large Systems Development",
+    year: 2020,
+    imgSrc: require("../img/portfolio/LSD.png"),
+    description: `In a group of 10 we were tasked to create a frontend and backend project. Inbetween those two
+    we had a Contract project with interfaces for both the backend and frontend. For both projects I set up Continous Integration,
+    for the backend Continous Deployment and for the frontend Continous Delivery. I also setup logging for the projects, and lastly I set up
+    monitoring using Prometheus and Grafana.`,
+    projectLink: "https://github.com/CBASoftwareDevolopment2020/LSD-Project",
+    sourceCode: "https://dev.azure.com/teamd-backend/lsd/_git/LSD-Frontend",
+  },
 ];
 
 export default softwareProjects;
