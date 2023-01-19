daf13a26000e167367a9572ab907312cb9a1aec4
Adam Saidane <adam.saidane@unity3d.com>
2023-01-19 22:13:46 +0100

    test

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,8 +12,7 @@ function Intro() {
 				<div className="Intro-text">
 					Hello, my name is Adam Saidane and I am currently studying for my MSc. in Games
 					Technology at the IT University of Copenhagen, Denmark. I am currently working on my thesis, and will finish my studies in 
-					June 2023. I previously worked as a software engineer student worker at 
-					Unity Technologies. I am currently looking for student opportunities in the games industry.
+					June 2023. I previously worked as a software engineer student worker at Unity Technologies. I am currently looking for student opportunities in the games industry.
 				</div>
 			</div>
 		</div>
