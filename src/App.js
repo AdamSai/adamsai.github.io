fb72e104ccea83102cbd33eb9bcf57c1f809ad92
Adam Saidane <adamsaidane252@gmail.com>
2023-04-24 18:14:41 +0200

    Icon hover color change and google analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Intro from './components/Intro';
 import Portfolio from './components/Portfolio';
+import ReactGA from 'react-ga';
 
 import './App.scss';
 import Footer from './components/Footer';
 
 function App() {
+	const initializeAnalytics = () => {
+		ReactGA.initialize('G-PQCZVERXDC');
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	};
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>Adam Saidane</h1>
+				{initializeAnalytics()}
 			</header>
 			<Intro />
 			<Portfolio />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import * as React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import itchioLogo from '../img/ui/itchio.png';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { ReactComponent as ReactLogo } from '../img/ui/itchio.svg';
 function Footer() {
 	return (
 		<div className="Footer">
@@ -12,17 +14,20 @@ function Footer() {
 					onClick={() => window.open('https://github.com/AdamSai')}
 					fontSize="large"
 				/>
+				<LinkedInIcon
+					className="Footer-icon"
+					onClick={() => window.open("https://www.linkedin.com/in/adam-saidane-2b4640182/")}
+					fontSize="large"
+				/>
 				<TwitterIcon
 					className="Footer-icon"
 					onClick={() => window.open('https://twitter.com/Predmada')}
 					fontSize="large"
 				/>
-				<img
-					onClick={() => window.open('https://predmada.itch.io')}
+				<ReactLogo onClick={() => window.open('https://predmada.itch.io')}
 					className="Footer-icon"
-					alt="itch.io logo"
-					src={itchioLogo}
-				/>
+					fontSize="large"
+					alt="itch.io logo" />
 			</div>
 		</div>
 	);
